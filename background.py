32a5f9d161fed136d5ba01be8c5010a030c2690f
Evgeny Blokhin <eb@tilde.pro>
2022-09-23 14:47:05 +0200

    Improve readability, avoid star imports

--- background.py
+++ background.py
@@ -1,47 +1,53 @@
-from scipy import stats
+
+from scipy import stats, interpolate
 import numpy as np
-from scipy import interpolate
+
 
 def background(intensities, theta, iterations=50, sec_iterations=50, curvature=0.000001, perc_anchor_pnts=50):
-        
+
     # Normalize the Intenstities of the input signal
-    MaxIntensity=max(intensities)
+    MaxIntensity = max(intensities)
     intensities = intensities / MaxIntensity
 
     # rebin the data to the number of anchor points passed
-    n_anchor_pnts = round(perc_anchor_pnts*len(intensities)/100)
+    n_anchor_pnts = round(perc_anchor_pnts * len(intensities) / 100)
     rebinned_bg = stats.binned_statistic(theta, intensities, statistic='median', bins=n_anchor_pnts)
     bg_theta = rebinned_bg.bin_edges
     bin_width = (bg_theta[1] - bg_theta[0])
-    bg_theta = bg_theta[0:bg_theta.size-1]
-    bg_theta = bg_theta + bin_width/2
+    bg_theta = bg_theta[0:bg_theta.size - 1]
+    bg_theta = bg_theta + bin_width / 2
     bg_intensities = rebinned_bg.statistic
 
     # determine the first background data
     bg = S_V_BG(bg_intensities, iterations, curvature)
 
-    i=0
+    i = 0
     while i < sec_iterations:
-        bg = (bg + S_V_BG(bg, iterations, curvature))/2
+        bg = (bg + S_V_BG(bg, iterations, curvature)) / 2
         bg[0] = bg_intensities[0]
         i += 1
-    
+
     # inter- and extrapolate the background data to fit the original x axis
     f = interpolate.interp1d(bg_theta, bg, bounds_error=False, fill_value="extrapolate")
     BackgroundIntensities = f(theta)
-    
+
     return np.array(BackgroundIntensities * MaxIntensity)
+
 
 def S_V_BG(intensities, iterations, curvature):
 
     background = intensities
-    i=0
-    j=1
+    i, j = 0, 1
+
     while i < iterations:
-        while j < background.size-1:
-            m = (background[j-1] + background[j+1] ) / 2
+
+        while j < background.size - 1:
+
+            m = (background[j - 1] + background[j + 1] ) / 2
             if background[j] > m + curvature:
                 background[j] = m
             j += 1
+
         i += 1
+
     return background
--- computeFOM.py
+++ computeFOM.py
@@ -1,26 +1,29 @@
-from math import sqrt
-from sre_constants import MAXGROUPS
-import numpy as np
+
 import math
-from XRDTools import *
+
+import numpy as np
+
+from XRDTools import returnDspacingFromTwoThetaAndWavelength
+
 
 def returnFOMArray(samplePattern, referencePatterns, Wavelength, maxPositionalDifference = 0.05, weightingAngle=0.5, weightingIntensity=0.5, weightingPhases=0.5):
 
-    # create an array of maximal positional differences which takes into account wavelength dispersion effects: tan(theta) 
-    numberOfPeaksInSamplePattern = round(np.size(samplePattern)/2)
+    # create an array of maximal positional differences which takes into account wavelength dispersion effects: tan(theta)
+    numberOfPeaksInSamplePattern = round(np.size(samplePattern) / 2)
     maxPositionalDifferenceArray = np.ndarray(numberOfPeaksInSamplePattern)
     maxPositionalDifferenceArray[:] = maxPositionalDifference
 
-    peak2ThetaList = samplePattern[0,:]
+    peak2ThetaList = samplePattern[0, :]
     peakDSpacingList = returnDspacingFromTwoThetaAndWavelength(peak2ThetaList, Wavelength)
-    peakIntensities = samplePattern[1,:]
+    peakIntensities = samplePattern[1, :]
 
-    i=0
+    i = 0
     for maxPositionalDifferenceValue in maxPositionalDifferenceArray:
         # maxPositionalDifferenceArray[i] = maxPositionalDifferenceArray[i] / ( ( math.cos( np.deg2rad( peak2ThetaList[i]/2 ))) ** 2 )
         # maxPositionalDifferenceArray[i] = maxPositionalDifferenceArray[i] * (Wavelength/2) * (math.cos( np.deg2rad( peak2ThetaList[i] ) ) )/( math.sin( np.deg2rad( peak2ThetaList[i] ) ) ** 2 )
         maxPositionalDifferenceArray[i] = maxPositionalDifference * peakDSpacingList[i]
-        i+=1
+        i += 1
+
     # Build the array for getting FOMs
     sampleDIerrors = np.array([peakDSpacingList, peakIntensities, maxPositionalDifferenceArray ])
 
@@ -28,42 +31,57 @@ def returnFOMArray(samplePattern, referencePatterns, Wavelength, maxPositionalDi
     FOMs = np.zeros(numberOfReferencePatterns)
     i = 0
     while i < numberOfReferencePatterns -1:
-        FOMs[i]=returnFOMOfTwoPatterns(sampleDIerrors,referencePatterns[i,:,:])
-        i+=1
+        FOMs[i] = returnFOMOfTwoPatterns(sampleDIerrors, referencePatterns[i, :, :])
+        i += 1
 
     return FOMs
 
+
 def returnFOMOfTwoPatterns(sampleDIerrors, referenceDIs, weightingAngle=0.5, weightingIntensity=0.5, weightingPhases=0.5):
-    
-    numberOfPeaksInReferencePattern = round(np.shape((np.nonzero(referenceDIs)))[1]/2)
-    referenceDIs = referenceDIs[0:numberOfPeaksInReferencePattern,:]
-    numberOfPeaksInSamplePattern = np.size(sampleDIerrors,0)
+
+    numberOfPeaksInReferencePattern = round(np.shape((np.nonzero(referenceDIs)))[1] / 2)
+    referenceDIs = referenceDIs[0:numberOfPeaksInReferencePattern, :]
+    numberOfPeaksInSamplePattern = np.size(sampleDIerrors, 0)
 
     # Create an array to hold the individual peak FOMs
-    FOM = np.zeros((numberOfPeaksInReferencePattern + numberOfPeaksInSamplePattern,4))
-    
-    
+    FOM = np.zeros((numberOfPeaksInReferencePattern + numberOfPeaksInSamplePattern, 4))
+
+
     # run through all experimental peaks and compute the FOM contribution of each peak
     if numberOfPeaksInReferencePattern > 0:
-        i=0
+        i = 0
         while i < numberOfPeaksInSamplePattern-1:
             positionDifference = np.abs(referenceDIs[:,0] - sampleDIerrors[0,i])
             minDifference = np.argmin(positionDifference)
+
             if positionDifference[minDifference] < sampleDIerrors[2,i]:
-                FOM[i,0:4]=returnPeakFOMS(sampleDIerrors[0:2,i],referenceDIs[minDifference,:])
-            i+=1
+                FOM[i,0:4]=returnPeakFOMS(sampleDIerrors[0:2, i], referenceDIs[minDifference, :])
+
+            i += 1
 
     maxPositionalDifference = sampleDIerrors.mean(axis=0)[2]
     numberOfAssignedPeaks = np.count_nonzero(FOM, axis=0)[0]
-    FOMTheta = 1-(FOM.sum(axis=0)[0])/(numberOfAssignedPeaks * maxPositionalDifference)
-    FOMIntensity = 1-(FOM.sum(axis=0)[1])/numberOfAssignedPeaks
-    FOMph = math.sqrt((FOM.sum(axis=0)[2]) * numberOfAssignedPeaks/((sampleDIerrors.sum(axis=0)[1])*numberOfPeaksInSamplePattern))
-    FOMdb = math.sqrt((FOM.sum(axis=0)[3]) * numberOfAssignedPeaks/((referenceDIs.sum(axis=0)[1])*numberOfPeaksInReferencePattern))
-    returnFOM = math.sqrt(FOMdb*(weightingAngle*FOMTheta+weightingIntensity*FOMIntensity+weightingPhases*FOMph)/(weightingAngle+weightingIntensity+weightingPhases))
-    
+
+    FOMTheta = 1 - (FOM.sum(axis=0)[0]) / (numberOfAssignedPeaks * maxPositionalDifference)
+    FOMIntensity = 1 - (FOM.sum(axis=0)[1]) / numberOfAssignedPeaks
+    FOMph = math.sqrt(
+        (FOM.sum(axis=0)[2]) * numberOfAssignedPeaks /
+        ((sampleDIerrors.sum(axis=0)[1]) * numberOfPeaksInSamplePattern)
+    )
+    FOMdb = math.sqrt(
+        (FOM.sum(axis=0)[3]) * numberOfAssignedPeaks /
+        ((referenceDIs.sum(axis=0)[1]) * numberOfPeaksInReferencePattern)
+    )
+    returnFOM = math.sqrt(
+        FOMdb *
+        (weightingAngle * FOMTheta + weightingIntensity * FOMIntensity + weightingPhases * FOMph) /
+        (weightingAngle + weightingIntensity + weightingPhases)
+    )
+
     return returnFOM
 
-def returnPeakFOMS(sampleDI,referenceDI):
+
+def returnPeakFOMS(sampleDI, referenceDI):
     FOMtheta = abs(sampleDI[0] - referenceDI[0])
     FOMintensity = abs(sampleDI[1] - referenceDI[1])
-    return np.array([FOMtheta,FOMintensity,sampleDI[1],referenceDI[1]])
+    return np.array([FOMtheta, FOMintensity, sampleDI[1], referenceDI[1]])
